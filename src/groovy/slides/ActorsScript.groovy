7d1c563866678dcc39c886262c39c3b3942ffd6d
Johannes Link <business@johanneslink.net>
2013-04-29 13:59:14 +0200

    Actors script from slides added

---
+++ src/groovy/slides/ActorsScript.groovy
@@ -0,0 +1,18 @@
+package slides
+
+import static groovyx.gpars.actor.Actors.*
+
+def printer   = reactor { println it }
+def decryptor = reactor { reply it.reverse() }
+
+actor {
+    decryptor << 'lellarap si yvoorG'
+    react { answer ->
+        printer << 'Decrypted message: ' + answer
+        decryptor.stop()
+        printer.stop()
+    }
+}.join()
+
+printer.join()
+decryptor.join()
