78ece8acdc1f62a6461a5236a97232682fcacb73
Johannes Link <business@johanneslink.net>
2013-05-07 21:59:46 +0200

    Added storehouse as clojure stm example

--- clojure/storehouse.clj
+++ clojure/storehouse.clj
@@ -1,16 +1,70 @@
-(ns storehouse)
+(defn empty-shelf [capacity] {:products '() :capacity capacity})
 
-(def a (ref 1))
+(defn put-in [shelf product]
+  (if (= (count (shelf :products )) (shelf :capacity ))
+    (throw (Exception. "Shelf is full."))
+    (assoc shelf :products (conj (shelf :products ) product))))
 
-(defn pv [a b c d] (println (list a b c d)))
+(defn take-out [shelf product]
+  (assoc shelf :products (remove #(= % product) (shelf :products ))))
 
-(add-watch a 'x pv)
+;(def s1 (empty-shelf 2))
+;(def s2 (put-in s1 :book ))
+;(def s3 (put-in s2 :printer ))
 
-(dosync
-  (alter a inc)
-  (alter a inc)
-  (dosync (alter a dec))
-  )
+;(println s3)
 
+;(def s4 (take-out s3 :book ))
+;(println s4)
 
+(defn empty-storehouse [] (ref {}))
 
+(defn add-shelf [map name shelf]
+  (assoc map name shelf))
+
+(defn replace-shelf [map name shelf]
+  (add-shelf map name shelf))
+
+(defn new-shelf [store name capacity]
+  (dosync
+    (alter store add-shelf name (empty-shelf capacity))))
+
+(defn update-shelf [store shelf-map]
+  (dosync
+    (let [shelf-name (first (keys shelf-map))
+          shelf (shelf-map shelf-name)]
+      (alter store replace-shelf shelf-name shelf))))
+
+(defn put-in-shelf [store shelf-name product]
+  (dosync
+    (let [changed-shelf (put-in (@store shelf-name) product)]
+      (update-shelf store {shelf-name changed-shelf}))))
+
+(defn take-from-shelf [store shelf-name product]
+  (dosync
+    (let [org-shelf (@store shelf-name)
+          changed-shelf (take-out org-shelf product)]
+      (if (= org-shelf changed-shelf)
+        (throw (Exception. (str "No such product in shelf: " product)))
+        (update-shelf store {shelf-name changed-shelf})))))
+
+(defn move [store product from-name to-name]
+  (dosync
+    (put-in-shelf store to-name product)
+    (take-from-shelf store from-name product)))
+
+(def store (empty-storehouse))
+(new-shelf store :a 2)
+(new-shelf store :b 3)
+(put-in-shelf store :a "a book")
+(put-in-shelf store :b "a lego block")
+
+(println @store)
+(println "Move book from :a to :b")
+(move store "a book" :a :b )
+(println @store)
+(println "Try to move a car from :a to :b")
+(try
+  (move store "a car" :a :b )
+  (catch Exception e (println "Move failed: ", (.getMessage e))))
+(println @store)
