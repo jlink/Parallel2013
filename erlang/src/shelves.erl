ec11d5fe96da76c6a79bc1dd86c082847893387c
Johannes Link <business@johanneslink.net>
2013-05-10 14:50:46 +0200

    added some erlang code for shelves

--- erlang/src/shelves.erl
+++ erlang/src/shelves.erl
@@ -1,11 +1,20 @@
 - module(shelves).
-- export([new_shelf/1, put_in/2, products/1, capacity/1]).
+- export([new_shelf/1, products/1, capacity/1]).
+- export([put_in/2, take_out/2]).
 %% - export([ call / 1 , create_shelf / 1]) .
 
 new_shelf(Capacity) -> {Capacity, []}.
 
 put_in(Shelf, Product) ->
-  [Product | Shelf].
+  {Capacity, Products} = Shelf,
+  if
+    Capacity =:= length(Products) -> throw({over_capacity});
+    true -> {Capacity, [Product | Products]}
+  end.
+
+take_out(Shelf, Product) ->
+  {Capacity, Products} = Shelf,
+  {Capacity, lists:delete(Product, Products)}.
 
 products({_, Products}) ->
   Products.
--- erlang/src/test.erl
+++ erlang/src/test.erl
@@ -11,7 +11,26 @@ storehouse_test() ->
 
   ?assert(Shelf =:= []).
 
-single_shelf_test() ->
+new_shelf_test() ->
   Shelf = shelves:new_shelf(3),
   ?assert(shelves:products(Shelf) =:= []),
   ?assert(shelves:capacity(Shelf) =:= 3).
+
+put_in_products_test() ->
+  Shelf = shelves:new_shelf(3),
+  Shelf1 = shelves:put_in(Shelf, book),
+  Shelf2 = shelves:put_in(Shelf1, magazine),
+  ?assertEqual([magazine, book], shelves:products(Shelf2)).
+
+put_in_over_capacity_test() ->
+  Shelf = shelves:new_shelf(1),
+  Shelf1 = shelves:put_in(Shelf, book),
+  ?assertException(throw, {over_capacity}, shelves:put_in(Shelf1, magazine)).
+
+take_out_products_test() ->
+  Shelf = shelves:new_shelf(3),
+  Shelf1 = shelves:put_in(Shelf, book),
+  Shelf2 = shelves:put_in(Shelf1, magazine),
+  Shelf3 = shelves:take_out(Shelf2, magazine),
+  Shelf3 = shelves:take_out(Shelf3, unknown),
+  ?assertEqual([book], shelves:products(Shelf3)).
